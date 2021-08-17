ee63cf201e385d88b2d7a96727929dec6c08ded1
ds13ojeda <diegojeda13@gmail.com>
2021-08-17 03:09:55 -0300

    Agrego carrito completo

--- app.js
+++ app.js
@@ -24,4 +24,8 @@ app.get("/login", (req,res)=>{
     res.sendFile(path.resolve("./views/login.html"))
 })
 
+app.get("/productCart",(req,res)=>{
+    res.sendFile(path.resolve("./views/productCart.html"))
+})
+
 
