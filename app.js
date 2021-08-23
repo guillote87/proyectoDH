13992235fc8011d3f4994cd7ffe1490151cdd874
guillote87 <guillermo27@gmail.com>
2021-08-23 18:13:43 -0300

    register html

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.listen(process.env.PORT||3000, ()=>{
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve("./views/index.html"))
 })
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"))
+})
 
 app.get("/productDetail",(req,res)=>{
     res.sendFile(path.resolve("./views/productDetail.html"))
