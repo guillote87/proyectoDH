b89fa930be0a18424d4c47519a9cfc2c5671256b
Lechufane <diego.villafane94@gmail.com>
2021-09-14 22:27:23 -0300

    Creando edit, delete y create

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -24,7 +24,7 @@ const productsController = {
     });
   },
   createView: (req, res) => {
-    res.render("products/createProducts");
+    res.render("about");
   },
   create: (req, res) => {
     let image;
