b720e9f8d094ab9ec320c1644228942fb3753d72
Lechufane <diego.villafane94@gmail.com>
2021-09-30 23:58:34 -0300

    CRUD de usuarios, register terminado

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const usersFilePath = path.join(__dirname, "../data/usersData.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
 const usersController = {
     login: (req, res) => {
         res.render("users/login");
     },
+    loginForm: (req, res) => {
+        console.log("falta el loginForm y el create");
+
+    },
     register: (req, res) => {
         res.render("users/register");
+    },
+    registerForm: (req, res) => {
+        let id = users.length + 1
+        let newUser = {
+            id: id == null? 1 : id,
+            password: req.body.pass1 == req.body.pass2 ? req.body.pass1 : "contraseña invalida",
+            name: req.body.name,
+            lastName: req.body.lastName,
+            image: req.file?.filename == undefined ? "defaultUser.png" : req.file.filename
+        }
+        users.push(newUser)
+        fs.writeFileSync(usersFilePath, JSON.stringify(users))
+        res.redirect("/")
     },
     cart: (req, res) => {
         res.render("users/productCart");
     },
+    //cartForm
 };
 
 module.exports = usersController;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,36 @@
-const express = require("express");
+const express = require("express")
 const router = express.Router();
+const multer = require('multer')
+const path = require('path');
+
 const usersController = require("../controllers/usersController");
 
+let multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        let folder = path.join(__dirname, "../../public/images/usuarios")
+        console.log(__dirname)
+        console.log(folder)
+        callback(null, folder)
+    },
+    filename: (req, file, callback) => {
+        console.log(file)
+        let image = Date.now() + path.extname(file.originalname)
+        console.log(image)
+        callback(null, image)
+    }
+})
+
+const fileUpload = multer({ storage: multerDiskStorage })
+
 router.get("/login", usersController.login);
+router.post("/login", usersController.loginForm)
+
+
 router.get("/register", usersController.register);
+router.post("/register", fileUpload.single('image'), usersController.registerForm)
+
 router.get("/cart", usersController.cart);
+//post de carrito
+
 
 module.exports = router;
