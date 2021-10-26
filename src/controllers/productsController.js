6c2e17a753cc26f1a000dc3d45f01d8fa98e87a1
guillote87 <guillermo27@gmail.com>
2021-10-26 12:04:38 -0300

    Edit de productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,104 +3,121 @@ const path = require("path");
 const db = require('../database/models')
 /* const productsFilePath = path.join(__dirname, "../data/productsData.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");*/     // DE CUANDO USABAMOS JSON
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");*/ // DE CUANDO USABAMOS JSON
 
 const productsController = {
-    detail: (req, res) => {
-        let id = req.params.id;
+        createView: (req, res) => {
+            db.Color.findAll()
+                .then((allColors) => {
+                    db.Category.findAll()
+                        .then((allCategories) => {
+                            db.Size.findAll()
+                                .then((allSizes) => {
+                                    res.render("products/createProducts", {
+                                        allColors,
+                                        allCategories,
+                                        allSizes
+                                    });
+                                })
+                        })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
 
-        db.Producto.findAll()
-            .then(interes => {
-                db.Producto.findByPk(id)
-                    .then(product => {
-                        res.render("products/productDetail", {
-                            product,
-                            interes
-                        });
-                    })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    },
-    filter: (req, res) => {
-        let filter = req.params.filter;
-        db.Producto.findAll()
-        .then(productos=>{
+        },
+        create: (req, res) => {
+            foto = req.file == undefined ? "default-image.png" : req.file.filename;
 
-        let product = productos.filter((productos) => productos.category == filter);
-        res.render("index", {
-            product,
-        });
-        })
-    },
-    createView: (req, res) => {
+            db.Producto.create({
+                    name: req.body.name,
+                    description: req.body.description,
+                    category: req.body.category,
+                    color: req.body.color,
+                    size: req.body.size,
+                    price: req.body.price,
+                    image: foto
+                })
+                .then(() => {
+                    res.redirect('/users/login');
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        detail: (req, res) => {
+            let id = req.params.id;
 
-        db.Color.findAll()
-            .then((allColors) => {
-                db.Category.findAll()
-                    .then((allCategories) => {
-                        db.Size.findAll()
-                            .then((allSizes) => {
-                                res.render("products/createProducts", {
-                                    allColors,
-                                    allCategories,
-                                    allSizes
-                                });
-                            })
-                    })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+            db.Producto.findAll()
+                .then(interes => {
+                    db.Producto.findByPk(id)
+                        .then(product => {
+                            res.render("products/productDetail", {
+                                product,
+                                interes
+                            });
+                        })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        },
+        filter: (req, res) => {
+            let filter = req.params.filter;
+            db.Producto.findAll()
+                .then(productos => {
 
-    },
-    create: (req, res) => {
-        foto = req.file == undefined ? "default-image.png" : req.file.filename;
+                    let product = productos.filter((productos) => productos.category == filter);
+                    res.render("index", {
+                        product,
+                    });
+                })
+        },
 
-        db.Producto.create({
-                name: req.body.name,
-                description: req.body.description,
-                category: req.body.category,
-                color: req.body.color,
-                size: req.body.size,
-                price: req.body.price,
-                image: foto
-            })
-            .then(() => {
-                res.redirect('/users/login');
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    },
-    editView: (req, res) => {
-        let id = req.params.id;
-      db.Producto.findByPk(id)
-      .then(product =>{
-        res.render("products/editProduct", {product});
-      })
-    },
-    editForm: (req, res) => {
-        let id = req.params.id;
-        products.forEach((prod) => {
-            if (prod.id == id) {
-                prod.name = req.body.name || prod.name;
-                prod.price = req.body.price || prod.price;
-                prod.category = req.body.category || prod.category;
-                prod.size = req.body.size || prod.size;
-                prod.color = req.body.color || prod.color;
-                prod.description = req.body.description ?
-                    req.body.description :
-                    prod.description;
-                prod.image = req.file == undefined ? prod.image : req.file.filename;
-                prod.image2 = req.file == undefined ? prod.image : req.file.filename;
-            }
-        });
-        let productsJSON = JSON.stringify(products, "utf-8");
-        fs.writeFileSync(productsFilePath, productsJSON);
-        return res.redirect("/products/" + id);
-    },
+        editView: (req, res) => {
+            let id = req.params.id;
+            db.Producto.findByPk(id)
+                .then((product) => {
+                    db.Color.findAll()
+                        .then((allColors) => {
+                            db.Category.findAll()
+                                .then((allCategories) => {
+                                    db.Size.findAll()
+                                        .then((allSizes) => {
+                                            res.render("products/editProduct", {
+                                                allColors,
+                                                allCategories,
+                                                allSizes,
+                                                product
+                                            });
+                                        })
+
+
+                                })
+                        })
+                })
+        },
+        editForm: (req, res) => {
+            let id = req.params.id;
+            db.Producto.update({
+                    name: req.body.name || prod.name,
+                    price : req.body.price || prod.price,
+                    category : req.body.category || prod.category,
+                    size : req.body.size || prod.size,
+                    color : req.body.color || prod.color,
+                    description : req.body.description ? req.body.description : prod.description,
+                    image : req.file == undefined ? prod.image : req.file.filename
+
+                },
+                {where :{id_productos: id}}
+            )
+            .then(()=>{
+                return res.redirect("/products/" + id);})
+                .catch(error => res.send(error))
+            
+},
+     
+    
     destroy: (req, res) => {
         id = req.params.id;
 
